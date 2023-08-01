41857251ef4a8a75720d59bd6b291580b318034a
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-08-01 22:48:34 +0500

    lesson22

---
+++ main.js
@@ -0,0 +1,73 @@
+// const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// const sortedAges = ages.sort((a, b) => a - b);
+
+// const total = ages.reduce((previousElement, currentElement) => previousElement + currentElement, 0);
+
+// const combinedAges = ages
+//     .map((item) => item * 2)
+//     .filter((item) => item >= 40)
+//     .sort((a, b) => a -b)
+//     .reduce((pre, curr) => pre + curr)
+
+
+// Homework
+
+// filter
+
+
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter(word => word.length > 6);
+
+
+// sort
+
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+
+// const array1 = [1, 30, 4, 21, 100000];
+
+
+// const sort = array1
+//     .sort((a, b) => b - a)
+
+
+
+// map
+
+// const array1 = [1, 4, 9, 16];
+
+// const map1 = array1.map(x => x * 2);
+
+
+// reduce
+
+
+// const array1 = [1, 2, 3, 4];
+
+// const Value = 0;
+// const result = array1.reduce(
+//   (pre, curr) => pre + curr, Value
+// );
+
+// forEach
+
+// const array1 = ['a', 'b', 'c'];
+
+// array1.forEach(element => console.log(element));
+
+
+// const staffsDetails = [
+//     { name: "Jam Josh", age: 44, salary: 4000, currency: "USD" },
+//     { name: "Justina Kap", age: 34, salary: 3000, currency: "USD" },
+//     { name: "Chris Colt", age: 37, salary: 3700, currency: "USD" },
+//     { name: "Jane Doe", age: 24, salary: 4200, currency: "USD" }
+//   ];
+
+
+// staffsDetails.forEach((staffDetail) => {
+//     let sentence = `I am ${staffDetail.name} a staff of Royal Suites.`;
+//     console.log(sentence);
+//   });
